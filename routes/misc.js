a387f075e484103a5c057f803ce57d11274f81aa
Angel Ruiz <aruizca@gmail.com>
2015-11-23 18:17:16 +1100

    Added preliminary support for file uploads

--- routes/misc.js
+++ routes/misc.js
@@ -1,4 +1,6 @@
-var router = require("express").Router(),
+var express = require("express"),
+  router = express.Router(),
+  app = require("../lib/app").getInstance(),
   renderer = require('../lib/renderer'),
   fs = require("fs"),
   models = require("../lib/models"),
@@ -6,10 +8,17 @@ var router = require("express").Router(),
 
 models.use(Git);
 
+// File Uploads handler
+var fileDestinationSubdir = '/assets';
+var fileDestinationDir = app.locals.config.get("application").repository + fileDestinationSubdir;
+var upload = multer({dest: fileDestinationDir});
+
 router.get("/misc/syntax-reference", _getSyntaxReference);
 router.post("/misc/preview", _postPreview);
 router.get("/misc/existence", _getExistence);
 router.get("/misc/upload-form", _getUploadForm);
+router.post("/misc/upload-file", upload.single('file'), _postUploadFile);
+app.use('/assets', express.static(fileDestinationDir));
 
 function _getSyntaxReference(req, res) {
   res.render('syntax');
@@ -47,6 +56,31 @@ function _getExistence(req, res) {
 
 function _getUploadForm(req, res) {
   res.render('upload-form');
+}
+
+function _postUploadFile(req, res, next) {
+  if (!res.locals.user) {
+    res.redirect('/login');
+  }
+
+  var fileName = req.file.originalname;
+  var newFile = app.locals.config.get("application").repository + fileDestinationSubdir + '/' + fileName;
+  var tempFile = req.file.path;
+
+  //TODO validate file does not already exist
+
+  fs.readFile(tempFile, function (err, data) {
+    fs.writeFile(newFile, data, function (err) {
+      fs.unlink(tempFile, function () {
+        var git_msg = req.body.fileMessage;
+        Git.add('..' + fileDestinationSubdir + '/' + fileName, git_msg, req.user.asGitAuthor, function (err) {
+        });
+        var responseMessage = 'File uploaded to: <pre>' + fileDestinationSubdir + '/' + fileName + '</pre>';
+        res.render('upload-form', {message: responseMessage});
+      });
+    });
+  });
+
 }
 
 router.all('*', function (req, res) {
