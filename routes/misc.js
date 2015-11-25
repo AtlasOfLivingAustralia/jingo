0ef682dc1e6b9657461c5737ff3539ec86ba5165
Angel Ruiz <aruizca@gmail.com>
2015-11-25 17:01:12 +1100

    Upgrades bootstrap tp 3.3.6 Validates if uploaded file already exists and requests user permission to overwrite Validates user has selected a file

--- routes/misc.js
+++ routes/misc.js
@@ -11,6 +11,7 @@ models.use(Git);
 // File Uploads handler
 var fileDestinationSubdir = '/assets';
 var fileDestinationDir = app.locals.config.get("application").repository + fileDestinationSubdir;
+var docSubdir = app.locals.config.get("application").docSubdir;
 var upload = multer({dest: fileDestinationDir});
 
 router.get("/misc/syntax-reference", _getSyntaxReference);
@@ -63,23 +64,51 @@ function _postUploadFile(req, res, next) {
     res.redirect('/login');
   }
 
-  var fileName = req.file.originalname;
-  var newFile = app.locals.config.get("application").repository + fileDestinationSubdir + '/' + fileName;
-  var tempFile = req.file.path;
+  if (req.file) {
+    var fileName = req.file.originalname;
+    var newFile = app.locals.config.get("application").repository + fileDestinationSubdir + '/' + fileName;
+    var tempFile = req.file.path;
 
-  //TODO validate file does not already exist
+    // TODO validate file extension
 
-  fs.readFile(tempFile, function (err, data) {
-    fs.writeFile(newFile, data, function (err) {
-      fs.unlink(tempFile, function () {
-        var git_msg = req.body.fileMessage;
-        Git.add('..' + fileDestinationSubdir + '/' + fileName, git_msg, req.user.asGitAuthor, function (err) {
+    //Validate file does not already exist
+    var overwriteFlag = req.body.overwriteFlag ? Boolean(req.body.overwriteFlag) : false;
+    if (overwriteFlag) {
+      // If the user has requested to overwrite the file
+      saveFileOnDisk();
+    } else {
+      fs.stat(newFile, function (err, stat) {
+        if (err == null) {
+          // If file exist
+          var responseMessage = 'There is already a file with the same name in the system.<br/><i class="fa fa-warning"></i> Please introduce the file details again and confirm that you want to overwrite the existing file.';
+          res.render('upload-form', {message: responseMessage, success: false, overwriteConfirmation: true});
+        } else if (err.code == 'ENOENT') {
+          // If file does not exist
+          saveFileOnDisk();
+        }
+      });
+    }
+  } else {
+    // No file attached to form submission
+    var responseMessage = 'Please make sure that you select a file to upload.';
+    res.render('upload-form', {message: responseMessage, success: false});
+  }
+  function saveFileOnDisk() {
+    fs.readFile(tempFile, function (err, data) {
+      fs.writeFile(newFile, data, function (err) {
+        fs.unlink(tempFile, function () {
+          var git_msg = req.body.fileMessage;
+          // TODO this code assumes that if the administrator chose to use a docs subdir, this will be one subdirectory level only
+          Git.add(docSubdir ? '..' : '' + fileDestinationSubdir + '/' + fileName, git_msg, req.user.asGitAuthor, function (err) {
+          });
+          var responseMessage = 'File uploaded to: <pre>' + fileDestinationSubdir + '/' + fileName + '</pre>';
+          res.render('upload-form', {message: responseMessage, success: true});
         });
-        var responseMessage = 'File uploaded to: <pre>' + fileDestinationSubdir + '/' + fileName + '</pre>';
-        res.render('upload-form', {message: responseMessage});
       });
     });
-  });
+  }
+
+
 
 }
 
